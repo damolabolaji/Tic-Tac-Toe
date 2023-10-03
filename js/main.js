0e09011f8aa1aa28cbe3bb6e22cf3218bcb896ca
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-03 11:46:26 +0100

    initialized repo and created gameboard functions

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const gameboard = document.querySelector("#gameboard");
+const infoDisplay = document.querySelector("#info");
+
+const startCells = ["", "", "", "", "", "", "", "", ""];
+
+function createBoard() {
+  startCells.forEach((_cell, index) => {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("square");
+    cellElement.id = index;
+    cellElement.addEventListener("click", addGo);
+    gameboard.append(cellElement);
+  });
+}
+
+createBoard();
+
+function addGo(e) {
+  //   console.log(e.target);
+  const goDisplay = document.createElement("div");
+  goDisplay.classList.add("cross");
+  e.target.append(goDisplay); //append the class of circle to whatever was clicked
+}
